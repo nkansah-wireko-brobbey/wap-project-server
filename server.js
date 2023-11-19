c5c42a14f59d526340a18932c10fbacf404ce737
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2023-11-19 15:28:58 -0600

    added cors for dev

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
 require('dotenv').config()
@@ -8,7 +9,7 @@ const userRoutes = require('./routes/user-routes')
 
 
 db()
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded())
 
